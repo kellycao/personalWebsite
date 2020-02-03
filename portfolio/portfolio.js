3dbc26eae6ae2ca4ebdf90b335d4f4c684dc5657
Yuxin Cao <55064994+kellycao@users.noreply.github.com>
2020-02-03 15:49:03 -0500

    test

---
+++ portfolio/portfolio.js
@@ -0,0 +1,76 @@
+
+var section2020_1 = document.getElementById('y2020_1');
+var section2019_1 = document.getElementById('y2019_1');
+var section2019_2 = document.getElementById('y2019_2');
+var section2019_3 = document.getElementById('y2019_3');
+var section2019_4 = document.getElementById('y2019_4');
+var section2017_1 = document.getElementById('y2017_1');
+var year=document.getElementsByClassName("year");
+
+section2017_1.onclick = function(){
+  var pdf = 'HMxGreenDesign_YuxinCao.pdf';
+  window.open('kellycao.github.io/personalWebsite/js/pdf/' + pdf, 'PDF');
+};
+
+// section2017_1.onmouseover= function(){
+//   year[0].style.animation="visib .4s forwards";
+// }
+
+// section2017_1.onmouseout= function(){
+//   year[0].style.animation="none";
+// }
+
+section2019_1.onclick = function(){
+  var pdf = 'MSTU4040_FinalProject_YuxinCao.pdf';
+  window.open('../js/pdf/web/viewer.html?file=http://127.0.0.1:3000/pdf/' + pdf, 'PDF');
+};
+
+// section2019_1.onmouseover= function(){
+//   year[1].style.animation="visib .4s forwards";
+// }
+//
+// section2019_1.onmouseout= function(){
+//   year[1].style.animation="none";
+// }
+
+section2019_2.onclick = function(){
+  var pdf = 'MSTU4083_FinalProject_YuxinCao.pdf';
+  window.open('../js/pdf/web/viewer.html?file=http://127.0.0.1:3000/pdf/' + pdf, 'PDF');
+};
+
+// section2019_2.onmouseover= function(){
+//   year[1].style.animation="visib .4s forwards";
+// }
+//
+// section2019_2.onmouseout= function(){
+//   year[1].style.animation="none";
+// }
+
+section2019_3.onclick = function(){
+  var pdf = 'AHA5060_FinalProject_YuxinCao.pdf';
+  window.open('../js/pdf/web/viewer.html?file=http://127.0.0.1:3000/pdf/' + pdf, 'PDF');
+};
+
+// section2019_3.onmouseover= function(){
+//   year[1].style.animation="visib .4s forwards";
+// }
+//
+// section2019_3.onmouseout= function(){
+//   year[1].style.animation="none";
+// }
+//
+// section2019_4.onmouseover= function(){
+//   year[1].style.animation="visib .4s forwards";
+// }
+//
+// section2019_4.onmouseout= function(){
+//   year[1].style.animation="none";
+// }
+//
+// section2020_1.onmouseover= function(){
+//   year[2].style.animation="visib .4s forwards";
+// }
+//
+// section2020_1.onmouseout= function(){
+//   year[2].style.animation="none";
+// }
